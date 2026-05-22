754e37188b8b57dfc8251bd79c0459a00123606e
inspflof <155272834+inspflof@users.noreply.github.com>
2026-05-22 09:36:48 +0200

    ✨ feat(slotList & navbar): add style

--- src/features/navbar/Navbar.tsx
+++ src/features/navbar/Navbar.tsx
@@ -1,8 +1,10 @@
 import { Link } from "react-router";
 
+import styles from "./Navbar.module.css"
+
 export default function Navbar() {
     return (
-        <nav>
+        <nav className={styles.navbar}>
             <Link to="/">
                 Home
             </Link>
