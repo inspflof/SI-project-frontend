inspflof/SI-project-frontend

commit 19d6fbd01662236a7300c6a8ab3c0e04c23519a4

🐞 fix(car): remove done tasks

src/features/dashboard/ParkingDashboard.tsx
@@ -9,6 +9,8 @@ export default function ParkingDashboard() {
     const { data: cars, loading: carLoading, error: carError } = useAllCar()
     const { error: recoverCarError, loading: recoverCarLoading, recoverCar } = useRecoverCar()
 
+    const carsFiltered = cars?.filter((car) => car.command?.[0].status !== "DONE")
+
     return (
         <div>
             <SlotList 
@@ -19,7 +21,7 @@ export default function ParkingDashboard() {
             <CarList 
                 error={carError}
                 loading={carLoading}
-                carList={cars}
+                carList={carsFiltered}
                 handleRecover={recoverCar}
                 recoverError={recoverCarError}
                 recoverLoading={recoverCarLoading}

commit 082d01351fe917e0298d2059695a4684d9dd8994

🐞 fix(car): display done retrieve tasks
still display done tasks when they are RETRIEVE type to recover it

Fix regression introduced by 19d6fbd

src/features/dashboard/ParkingDashboard.tsx
@@ -9,7 +9,7 @@ export default function ParkingDashboard() {
     const { data: cars, loading: carLoading, error: carError } = useAllCar()
     const { error: recoverCarError, loading: recoverCarLoading, recoverCar } = useRecoverCar()
 
-    const carsFiltered = cars?.filter((car) => car.command?.[0].status !== "DONE")
+    const carsFiltered = cars?.filter((car) => car.command?.[0].status !== "DONE" || car.command?.[0].type !== "RETRIEVE")
 
     return (
         <div>